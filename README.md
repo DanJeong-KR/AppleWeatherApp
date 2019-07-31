7c2d0d301bdf89a6851c52c7967fd4c2f3fc661b
changSic <a920506@gmail.com>
2019-07-31 23:31:20 +0900

    docs : Initialize Repo

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Kakaopay_internship
 카카오페이 사전과제 weather app repo
+
+### * Apple Weather App
+
+### * [Dark Sky API](https://darksky.net/dev/docs)
+
+### 필수 구현 사항
+* Networking
+  * Model & Parser
+  * API의 응답을 이용한 날씨 정보 화면 구현 검색
+* Location
+  * MapKit을 이용해서 도시명 검색
+  * MapKit을 통해 얻어낸 검색된 도시의 위/경도 좌표로 날씨 API 호출 검색된 도시정보를 **앱 재시작 시**, 반영될 수 있도록 구현
---
+++ WeatherApp_KakaoPay/WeatherApp_KakaoPay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp_KakaoPay
+//
+//  Created by Sicc on 31/07/2019.
+//  Copyright © 2019 chang sic jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
